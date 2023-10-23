196204514cc81f4b60fc54d3f3c081a13d403f33
NadaaNasserr <148685934+NadaaNasserr@users.noreply.github.com>
2023-10-23 21:24:00 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,117 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner S = new Scanner(System.in);
+
+
+
+        //Exercise1
+
+//        System.out.println("pleas Enter weight(kg) = " );
+//        int weight = S.nextInt();
+//
+//        System.out.println("pleas Enter height(m) = " );
+//        float height = S.nextFloat();
+//
+//        double bmi = weight / (height * height) ;
+//        System.out.println("BMI = " + bmi );
+
+
+
+             //Exercise2
+
+//        System.out.println("Obtained Marks = " );
+//        int obtainedMarks  = S.nextInt();
+//
+//        System.out.println("Total Marks = " );
+//        int totalMarks = S.nextInt();
+//
+//        double percentage = (obtainedMarks * 100 ) / totalMarks;
+//        System.out.println("Percentage = " + percentage+"%" );
+
+
+
+            //Exercise3
+
+//        System.out.println("Amount is USD = " );
+//        int usd  = S.nextInt();
+
+//        double exchange = usd * 0.85;
+//        System.out.println("Amount in EUR = " + exchange );
+
+
+
+                //Exercise4
+
+//        System.out.println("pleas Enter input String: " );
+//        String input  = S.nextLine();
+//        StringBuilder reverseInput = new StringBuilder(input);
+//        System.out.println("Length of the String: " + input.length() + " And Reversed Strint: " + reverseInput.reverse() );
+
+
+
+            //Exercise5
+//        System.out.println("Input: Sentence =  " );
+//        String sentence  = S.nextLine();
+//
+//        System.out.println("Start index = ");
+//        int number1  = S.nextInt();
+//
+//        System.out.println("end index = ");
+//        int number2 = S.nextInt();
+//
+//        System.out.print(sentence.substring(number1,number2));
+
+
+
+            // Exercise6
+
+//        System.out.println("Input: Sentence =  " );
+//        String sentence  = S.nextLine();
+//
+//        System.out.println("Keyword =  " );
+//        String keyword  = S.nextLine();
+//
+//        if (sentence.contains(keyword) == true )
+//        System.out.println("Keyword "+ keyword +" is present in the sentence.");
+//
+//        else
+//            System.out.println("Keyword  "+ keyword + " is not present in the sentence.");
+
+
+
+            //Exercise7
+
+//        System.out.println("Input: Sentence =  " );
+//        String input  = S.nextLine();
+//
+//        System.out.println("Word to Replace =  " );
+//        String input1  = S.nextLine();
+//
+//        System.out.println("Replacement Word = " );
+//        String input2  = S.nextLine();
+//
+//        System.out.println(input.replace(input1,input2));
+
+
+        
+             //Exercise8
+
+//        System.out.println("Input: String 1 =  " );
+//         String input  = S.nextLine();
+//
+//        System.out.println("Input: String 2 =  " );
+//        String input2  = S.nextLine();
+//
+//        if (input.equalsIgnoreCase(input2) == true)
+//        System.out.println("Strings are equal (ignoring case)");
+//
+//        else
+//            System.out.println("Strings are not equal ");
+
+
+    }
+}
